e08b17be97a614a77be29a36141cd62565c1bc76
Alejandro Martinez <alecsmartinez5@gmail.com>
2024-08-18 18:04:24 -0300

    Se agregan ejercicios JAVA
    Co-Authored-By: Lautaro Martinez <86857156+lauemartinez@users.noreply.github.com>

---
+++ Alejandro Martinez/JAVA/WhileEnJava/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int contraseña;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese la contraseña (es 123): ");
+        contraseña = input.nextInt();
+
+        while(contraseña != 123) {
+            System.out.println("ERROR... Reingrese la contraseña (es 123): ");
+            contraseña = input.nextInt();
+        }
+        System.err.println("Usted ha ingresado al sistema!!!");
+        input.close();
+    }
+}
