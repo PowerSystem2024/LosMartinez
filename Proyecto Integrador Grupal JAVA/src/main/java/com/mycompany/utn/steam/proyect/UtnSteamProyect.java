5ae881b05b4da084313c1f3e052ac3f958ac6101
Lautaro Martinez <lautipro2006@gmail.com>
2024-09-12 22:10:59 -0300

    #4 Se agrega un servicio para el display de menu, modelo de menus y se pone un ejemplo en el main

--- Proyecto Integrador Grupal JAVA/src/main/java/com/mycompany/utn/steam/proyect/UtnSteamProyect.java
+++ Proyecto Integrador Grupal JAVA/src/main/java/com/mycompany/utn/steam/proyect/UtnSteamProyect.java
@@ -2,9 +2,50 @@ package com.mycompany.utn.steam.proyect;
 
 import com.mycompany.utn.steam.model.User;
 import com.mycompany.utn.steam.service.impl.UserServiceImpl;
+import com.mycompany.utn.steam.util.MenuDisplayer;
+import java.util.Scanner;
 
 public class UtnSteamProyect {
     public static void main(String[] args) {
+        displayHomeMenu();
+        // displayAllUsersTest();
+    }
+    
+    private static void displayHomeMenu(){
+        Scanner scanner = new Scanner(System.in);
+        MenuDisplayer.displayMenu("home");
+        
+        int option = -1;
+        while (option != 4) {    
+            try {
+                option = Integer.parseInt(scanner.nextLine());
+
+                switch (option) {
+                    case 1:
+                        System.out.println("Has seleccionado la Opcion 1");
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado la Opcion 2");
+                        break;
+                    case 3:
+                        System.out.println("Has seleccionado la Opcion 3");
+                        break;
+                    case 4:
+                        System.out.println("Saliendo...");
+                        break;
+                    default:
+                        System.out.println("Opción no válida, por favor intente de nuevo.");
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada no válida, por favor ingrese un número.");
+            }
+        }
+        
+        scanner.close();
+    }
+    
+    private static void displayAllUsersTest(){
         // Iniciamos el userService
         UserServiceImpl userService = new UserServiceImpl();
         
@@ -17,10 +58,9 @@ public class UtnSteamProyect {
         
         // Hago un foreach y lo muestro
         for (User user : users) {
-            System.out.println(user.getId());
-            System.out.println(user.getName());
-            System.out.println(user.getEmail());
-            System.out.println("----------------");
+            MenuDisplayer.displayLine(user.getId()+"", false, false);
+            MenuDisplayer.displayLine(user.getName(), false, false);
+            MenuDisplayer.displayLine(user.getEmail(), false, true);
         }
     }
 }
--- Proyecto Integrador Grupal JAVA/src/main/java/com/mycompany/utn/steam/util/JsonDataLoader.java
+++ Proyecto Integrador Grupal JAVA/src/main/java/com/mycompany/utn/steam/util/JsonDataLoader.java
@@ -1,9 +1,10 @@
 package com.mycompany.utn.steam.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mycompany.utn.steam.model.Menu;
 import com.mycompany.utn.steam.model.User;
-import java.io.IOException;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
@@ -13,6 +14,20 @@ public class JsonDataLoader {
             ObjectMapper mapper = new ObjectMapper();
             InputStream is = JsonDataLoader.class.getResourceAsStream("/data/users.json");
             return mapper.readValue(is, mapper.getTypeFactory().constructCollectionType(List.class, User.class));
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading users data", e);
+        }
+    }
+    
+    public static Menu getMenuData(String menuSlug) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            InputStream is = JsonDataLoader.class.getResourceAsStream("/data/menu-options.json");
+            List<Menu> allMenus = mapper.readValue(is, mapper.getTypeFactory().constructCollectionType(List.class, Menu.class));
+            return allMenus.stream()
+                           .filter(menu -> menu.getSlug().equals(menuSlug))
+                           .findFirst()
+                           .orElseThrow(() -> new IllegalArgumentException("ERROR: Menu slug not found in menu-options.json"));
         } catch (IOException e) {
             throw new RuntimeException("Error loading users data", e);
         }
